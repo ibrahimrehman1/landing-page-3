5653a7cd1affa27bf10c60dbfa7364fe6ffc167f
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2021-01-24 20:35:55 +0500

    Added Accordion using Javascript

--- app.js
+++ app.js
@@ -3,11 +3,46 @@ window.addEventListener("scroll", (e)=>{
     if (window.scrollY){
         nav.style.background = "linear-gradient(to bottom, #ae85fe 0%,#8666ff 0%,#ae85fe 0%,#8666ff 100%)"
         nav.style.boxShadow = "0 3px 15px 0 rgba(0, 0, 0, .15)"
-        nav.style.padding = "0.7em 1.2em"
+        setTimeout(()=>{nav.style.padding = "0.7em 1.2em"}, 200)
+        
     }
     else{
         nav.style.background = "transparent"
         nav.style.boxShadow = "none"
-        nav.style.padding = "1.2em"
+        setTimeout(()=>nav.style.padding = "1.2em", 200)
+
     }
+})
+
+window.addEventListener("load", ()=>{
+    document.querySelector("#contact-list").classList.add("add-style")
+    document.querySelector("#show-p-1").classList.add("show-p");
+})
+
+document.querySelector("#contact-list").addEventListener("click", ()=>{
+    document.querySelector("#contact-list").classList.toggle("add-style")
+    document.querySelector("#show-p-1").classList.toggle("show-p");
+    document.querySelector("#show-p-2").classList.remove("show-p");
+    document.querySelector("#show-p-3").classList.remove("show-p");
+    document.querySelector("#message-editing").classList.remove("add-style");
+    document.querySelector("#conversation-analytics").classList.remove("add-style");
+
+})
+
+document.querySelector("#message-editing").addEventListener("click", ()=>{
+    document.querySelector("#message-editing").classList.toggle("add-style")
+    document.querySelector("#show-p-2").classList.toggle("show-p");
+    document.querySelector("#show-p-1").classList.remove("show-p");
+    document.querySelector("#show-p-3").classList.remove("show-p");
+    document.querySelector("#contact-list").classList.remove("add-style");
+    document.querySelector("#conversation-analytics").classList.remove("add-style");
+})
+
+document.querySelector("#conversation-analytics").addEventListener("click", ()=>{
+    document.querySelector("#conversation-analytics").classList.toggle("add-style")
+    document.querySelector("#show-p-3").classList.toggle("show-p");
+    document.querySelector("#show-p-1").classList.remove("show-p");
+    document.querySelector("#show-p-2").classList.remove("show-p");
+    document.querySelector("#contact-list").classList.remove("add-style");
+    document.querySelector("#message-editing").classList.remove("add-style");
 })
