b944efe754626f3e4590eee455f2bfdbc42b0db0
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2021-01-24 18:39:16 +0500

    Added Transitions to Navbar

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ window.addEventListener("scroll", (e)=>{
     if (window.scrollY){
         nav.style.background = "linear-gradient(to bottom, #ae85fe 0%,#8666ff 0%,#ae85fe 0%,#8666ff 100%)"
         nav.style.boxShadow = "0 3px 15px 0 rgba(0, 0, 0, .15)"
+        nav.style.padding = "0.7em 1.2em"
     }
     else{
         nav.style.background = "transparent"
         nav.style.boxShadow = "none"
-
-
+        nav.style.padding = "1.2em"
     }
 })
