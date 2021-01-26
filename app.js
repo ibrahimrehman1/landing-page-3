c4c0df90eac4410033522781cd94a59ece90ed2d
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2021-01-27 03:26:36 +0500

    Added Hamburger Icon

--- app.js
+++ app.js
@@ -1,38 +1,61 @@
+let state = false;
 window.addEventListener("scroll", (e)=>{
     let nav = document.querySelector("nav");
     if (window.scrollY){
         nav.style.background = "linear-gradient(to bottom, #ae85fe 0%,#8666ff 0%,#ae85fe 0%,#8666ff 100%)"
         nav.style.boxShadow = "0 3px 15px 0 rgba(0, 0, 0, .15)"
-        setTimeout(()=>{nav.style.padding = "0.7em 1.2em"}, 200)
+        document.querySelector(".move-top").classList.add("move-top-show");
+        setTimeout(()=>{
+            nav.style.padding = "0.7em 1.2em";
+            document.querySelector(".hamburger-icon").style.top = "12px";
+        }, 200)
         
     }
     else{
         nav.style.background = "transparent"
+        document.querySelector(".move-top").classList.remove("move-top-show");
         nav.style.boxShadow = "none"
-        setTimeout(()=>nav.style.padding = "1.2em", 200)
-
+        setTimeout(()=>{
+            nav.style.padding = "1.2em";
+            document.querySelector(".hamburger-icon").style.top = "14px";
+        }, 200)
+        if (state){
+            document.querySelector("#hidden-div").classList.remove("show-div")
+            document.querySelector("#bar-1").classList.remove("tilt-1");
+            document.querySelector("#bar-2").classList.remove("tilt-2");
+            document.querySelector("#bar-3").classList.remove("tilt-3");
+            document.querySelector("#nav-links").classList.remove("links");
+            nav.classList.remove("navbar-extend");
+            state = false;
+        }
     }
 })
 
-// window.addEventListener("load", ()=>{
-//     document.querySelector("#contact-list").classList.add("add-style")
-// })
 
-// document.querySelector("#contact-list").addEventListener("click", ()=>{
-//     document.querySelector("#contact-list").classList.toggle("add-style");
-//     document.querySelector("#message-editing").classList.remove("add-style");
-//     document.querySelector("#conversation-analytics").classList.remove("add-style");
+document.querySelector("#burgerIcon").addEventListener("click", ()=>{
+    let nav = document.querySelector(".navbar");
+    document.querySelector("#bar-1").classList.toggle("tilt-1");
+    document.querySelector("#bar-2").classList.toggle("tilt-2");
+    document.querySelector("#bar-3").classList.toggle("tilt-3");
+    document.querySelector("#nav-links").classList.toggle("links");
+    document.querySelector("#hidden-div").classList.toggle("show-div")
+    nav.classList.toggle("navbar-extend");
+    nav.style.background = "linear-gradient(to bottom, #ae85fe 0%,#8666ff 0%,#ae85fe 0%,#8666ff 100%)"
+    nav.style.boxShadow = "0 3px 15px 0 rgba(0, 0, 0, .15)"
+    document.querySelector(".move-top").classList.add("move-top-show");
+    setTimeout(()=>{
+        nav.style.padding = "0.7em 1.2em";
+        document.querySelector(".hamburger-icon").style.top = "12px";
+    }, 200)
+    state = true;
 
-// })
+})
 
-// document.querySelector("#message-editing").addEventListener("click", ()=>{
-//     document.querySelector("#message-editing").classList.toggle("add-style")
-//     document.querySelector("#contact-list").classList.remove("add-style");
-//     document.querySelector("#conversation-analytics").classList.remove("add-style");
-// })
-
-// document.querySelector("#conversation-analytics").addEventListener("click", ()=>{
-//     document.querySelector("#conversation-analytics").classList.toggle("add-style")
-//     document.querySelector("#contact-list").classList.remove("add-style");
-//     document.querySelector("#message-editing").classList.remove("add-style");
-// })
+function minimize(){
+    document.querySelector("#hidden-div").classList.remove("show-div")
+    document.querySelector("#bar-1").classList.remove("tilt-1");
+    document.querySelector("#bar-2").classList.remove("tilt-2");
+    document.querySelector("#bar-3").classList.remove("tilt-3");
+    document.querySelector("#nav-links").classList.remove("links");
+    document.querySelector(".navbar").classList.remove("navbar-extend");
+}
